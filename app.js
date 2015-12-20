a31bb4dae1381d05c0ba6dd261b995587fb8e63a
franz <pig3790@gmail.com>
2015-12-20 18:41:00 +0800

    Inital build uses html audio tag, plan to migrate to web audio api soon

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require('express'),
+    app = express();
+var fs = require('fs');
+app.use(express.static(__dirname + '/public'));
+var id3 = require('id3js');
+
+
+
+app.get('/lists', function (req, res) {
+    fs.readdir(__dirname + '/music', function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        result.forEach(function (fileNane, index) {
+            if (fileNane == "desktop.ini") {
+                result.splice(index, 1);
+
+            }
+            result[index] = result[index].substr(0, result[index].lastIndexOf('.')) || result[index];
+        })
+
+        res.send(result);
+    });
+
+});
+
+
+app.get('/music/:id', function (req, res) {
+
+    var asset = decodeURIComponent(req.url);
+    res.download('.' + asset);
+
+})
+
+app.get('/metaData/:id', function (req, res) {
+
+    id3({
+        file: './music/Kalimba.mp3',
+        type: id3.OPEN_LOCAL
+    }, function (err, tags) {
+        // tags now contains your ID3 tags
+        res.send(tags)
+    });
+
+
+})
+
+
+
+
+
+
+app.listen(3000);
---
+++ public/js/app.js
@@ -0,0 +1,86 @@
+$.ajax({
+    url: './lists',
+    success: function (res) {
+
+        loop(res);
+    }
+});
+var forPaused = false,
+    lastIndex, hasListener = false;
+
+function loop(arrayMusic) {
+    var tableString = "";
+    for (var i = 0; i < arrayMusic.length; i++) {
+        tableString += '<tr><td>'
+        tableString += arrayMusic[i];
+        tableString += '</td></tr>';
+    }
+    document.querySelector("#list").innerHTML = tableString;
+    var items = document.querySelectorAll("#list td");
+      [].forEach.call(items, click);
+}
+
+function click(element, index, array) {
+    element.addEventListener("click", function (event) {
+
+        if (forPaused == true && lastIndex == this) {
+            pause();
+        } else {
+            play(this.textContent || this.innerText, this);
+        }
+    })
+}
+
+function play(asset, elem) {
+    killClass(["activeMusicListItem", "progress"]);
+    changeClass(elem, "progress");
+    document.querySelector("h1").innerHTML = asset;
+
+    if (lastIndex != elem) {
+        var link = './music/' + asset + '.mp3';
+        document.querySelector('#music').src = link;
+        lastIndex = elem;
+    }
+
+    document.querySelector('#music').oncanplaythrough = function () {
+        hasListener = true;
+        playing(elem);
+    }
+    if (hasListener) {
+        playing(elem);
+    }
+}
+
+function playing(elem) {
+    document.querySelector("#music").play();
+    changeClass(elem, "activeMusicListItem");
+    forPaused = true;
+}
+
+function pause() {
+    killClass("activeMusicListItem");
+    document.querySelector("#music").pause();
+    forPaused = false;
+}
+
+
+
+
+function changeClass(elem, applyClass) {
+    elem.setAttribute("class", applyClass)
+}
+
+
+
+function killClass(findClasses) {
+    if (typeof findClasses === 'string') {
+        findClasses = [findClasses];
+    }
+
+    for (var i = 0; i < findClasses.length; i++) {
+        var set = document.querySelectorAll("." + findClasses[i]);
+            [].forEach.call(set, function (item) {
+            item.setAttribute("class", "");
+        });
+    }
+}
